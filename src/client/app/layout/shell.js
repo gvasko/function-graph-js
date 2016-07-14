da9753906cb8079447f02a23067d9fd742c09581
Gabor Vasko-Szedlar <gabor.vaskoszedlar@gmail.com>
2016-07-14 12:24:05 +0000

    Playing with creating and removing items

--- src/client/app/layout/shell.js
+++ src/client/app/layout/shell.js
@@ -8,18 +8,41 @@
     Shell.$inject = ['$scope'];
     
     function Shell($scope) {
+        $scope.nextFruitId = 0;
+        
+        $scope.fruitList = [];
+        
+        $scope.prototypes = ['Apple', 'Banana', 'Appricot', 'Plum', 'Strawberry'];
         
         $scope.removeFruit = function(id) {
             console.log('Remove fruit: ' + id);
-        }
+            //var  = $.grep($scope.fruitList, 
+            var index = findFirstIndex($scope.fruitList, function(item) {
+                return item.id == id;
+            });
+            $scope.fruitList.splice(index, 1);
+        };
         
+        function findFirstIndex(array, func) {
+            for (var i = 0; i < array.length; i++) {
+                if (func(array[i])) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
         $scope.removeAllFruits = function() {
             console.log('Remove all fruits');
-        }
+            $scope.fruitList = [];
+        };
         
         $scope.buyNewFruit = function() {
             console.log('Buy new fruit');
-        }
+            var protoIndex = Math.floor(Math.random() * $scope.prototypes.length);
+            $scope.fruitList.push({id: $scope.nextFruitId, name: $scope.prototypes[protoIndex] + $scope.nextFruitId});
+            $scope.nextFruitId++;
+        };
         
     }
     
