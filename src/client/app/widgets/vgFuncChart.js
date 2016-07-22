1e83f7c90f6d1ec81fe2e2c8f39024347fff7055
Gabor Vasko-Szedlar <gabor.vaskoszedlar@gmail.com>
2016-07-22 07:33:21 +0000

    Fix chart update

--- src/client/app/widgets/vgFuncChart.js
+++ src/client/app/widgets/vgFuncChart.js
@@ -31,17 +31,45 @@
                     xAxis: {
                         categories: scope.categories
                     },
-                    series: scope.funcValues
+                    series: []
                 });
                 scope.$watch('funcValues', function(newValue) {
-                    //chart.series.push(newValue[0]);
-                    //chart.series.push(newValue[1]);
-                    //chart.redraw();
+                    deleteMissingSeries(chart, newValue);
+                    addNewSeries(chart, newValue);
+                    updateExistingSeries(chart, newValue);
                     console.log('vgFuncChart updated');
                 }, true);
             }
         };
         return directive;
+    }
+    
+    function deleteMissingSeries(chart, newSeriesList) {
+        chart.series.forEach(function(currentOrig) {
+            var newSeries = newSeriesList.find(function(currentNew) { return currentOrig.name === currentNew.name });
+            if (newSeries === undefined) {
+                currentOrig.remove();
+            }
+        });
+    }
+    
+    function addNewSeries(chart, newSeriesList) {
+        newSeriesList.forEach(function(currentNew) {
+            var existingSeries = chart.series.find(function(currentOrig) { return currentOrig.name === currentNew.name });
+            if (existingSeries === undefined) {
+                chart.addSeries(currentNew);
+            }
+        });
+    }
+    
+    function updateExistingSeries(chart, newSeriesList) {
+        chart.series.forEach(function(currentOrig) {
+            var newSeries = newSeriesList.find(function(currentNew) { return currentOrig.name === currentNew.name });
+            if (newSeries === undefined) {
+                throw 'Series not found';
+            }
+            currentOrig.setData(newSeries.data);
+        });
     }
     
 })();
